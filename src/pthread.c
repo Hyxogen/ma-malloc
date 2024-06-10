683751abea47914281307a75b2f74d85b4e801e6
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2024-06-10 17:20:58 +0200

    format code

--- src/pthread.c
+++ src/pthread.c
@@ -3,19 +3,9 @@
 #if MA_USE_PTHREAD
 #include <pthread.h>
 
-int ma_init_mutex(ma_mtx *mtx)
-{
-	return pthread_mutex_init(mtx, NULL);
-}
+int ma_init_mutex(ma_mtx *mtx) { return pthread_mutex_init(mtx, NULL); }
 
-int ma_lock_mutex(ma_mtx *mtx)
-{
-	return pthread_mutex_lock(mtx);
+int ma_lock_mutex(ma_mtx *mtx) { return pthread_mutex_lock(mtx); }
 
-}
-
-int ma_unlock_mutex(ma_mtx *mtx)
-{
-	return pthread_mutex_unlock(mtx);
-}
+int ma_unlock_mutex(ma_mtx *mtx) { return pthread_mutex_unlock(mtx); }
 #endif
