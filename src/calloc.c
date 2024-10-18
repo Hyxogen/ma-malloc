652df076680661f34d6bae023dd12e7163ee0610
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2024-10-18 11:22:11 +0200

    fix possible race when getting chunk size
    As the header for chunks is packed tightly and stores information about
    the previous chunk, we should lock the arena when accessing it, even if
    it's just a read.

--- src/calloc.c
+++ src/calloc.c
@@ -13,15 +13,23 @@ void *ma_calloc(size_t nmemb, size_t size)
 	if (!ma_check_requestsize(total))
 		return NULL;
 
-	void *p = ma_malloc(total);
+	struct ma_arena *arena = ma_get_current_arena();
+	ma_lock_arena(arena);
+
+	void *p = ma_malloc_no_lock(arena, total);
 
 	if (p) {
 		struct ma_hdr *chunk = ma_mem_to_chunk(p);
 
 		// watch out, the chunk might have been perturbed, so we don't
 		// know for sure if it has been zeroed
+		
+		// FIXME: we don't have to be locked when zeroing the memory,
+		// only just when getting the size
 		ft_memset(p, 0, ma_get_size(chunk));
 	}
+
+	ma_unlock_arena(arena);
 
 	ma_dump_print("void *tmp_%p = ma_calloc(%zu, %zu);\n", p, nmemb, size);
 	return p;
